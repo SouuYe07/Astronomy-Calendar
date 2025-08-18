89a8457fe945f7677450b60d3c4ada45316fc754
SouuYe07 <iesouaguila07@gmail.com>
2025-08-18 14:15:10 +0800

    Revert "Added events API"
    This reverts commit 967b7e7a38ebbee6c03081377664219d541e0ad0.

--- fetch.js
+++ fetch.js
@@ -1,24 +1,5 @@
 const api_key = `0bf6EN8WNOeqeiEo4mGyMu40BEk21nLsl5dU47o4`;
 
-
-async function events(month){
-    
-    try{
-        fetch (`https://ll.thespacedevs.com/2.3.0/events/?month=4&format=json&limit=100`)
-
-        if(!response.ok){
-            throw new Error("Could not fetch events resource")
-        }
-
-        const data = await response.json()
-        console.log(data)
-    }
-    catch(error){
-        console.error(error)
-    }
-
-    }
-
 // Astronomy Picture of the Day
 async function apod(date){
 
@@ -26,7 +7,7 @@ async function apod(date){
         const response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`)
 
         if (!response.ok){
-            throw new Error("Could not fetch apod resource")
+            throw new Error("Could not fetch resource")
         }
 
         const data = await response.json()
@@ -45,7 +26,7 @@ async function asteroid(date){
         const response = await fetch (`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${api_key}`)
 
         if (!response.ok){
-            throw new Error("Could not fetch asteroid resource")
+            throw new Error("Could not fetch resource")
         }
 
         const data = await response.json()
@@ -57,3 +38,21 @@ async function asteroid(date){
     }
 }
 
+// Space Weather Database Of Notifications, Knowledge, Information (DONKI)
+async function donki(date){
+
+    try{
+        const response = await fetch (`https://api.nasa.gov/DONKI/FLR?startDate=${date}&endDate=${date}&api_key=${api_key}`)
+
+        if (!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+
+        const data = await response.json()
+        console.log(data)
+    }
+
+    catch(error){
+        console.error(error)
+    }
+}
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ testDate = "2025-01-01";
 
 apod(testDate);
 asteroid(testDate);
-events(4);
+donki(testDate);
