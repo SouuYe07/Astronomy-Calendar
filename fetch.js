06965fe8fff832c56042f7d71ef7dd7c75b349cd
SouuYe07 <iesouaguila07@gmail.com>
2025-08-18 11:07:23 +0800

    Asynced API fetches

--- fetch.js
+++ fetch.js
@@ -1,25 +1,58 @@
 const api_key = `0bf6EN8WNOeqeiEo4mGyMu40BEk21nLsl5dU47o4`;
 
 // Astronomy Picture of the Day
-function apod(date){
-    fetch (`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+async function apod(date){
+
+    try{
+        const response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`)
+
+        if (!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+
+        const data = await response.json()
+        console.log(data)
+    }
+
+    catch(error){
+        console.error(error)
+    }
 }
 
 // Asteroid Near Earth Object Web Service
-function asteroid(date){
-    fetch (`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${api_key}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+async function asteroid(date){
+
+    try{
+        const response = await fetch (`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${api_key}`)
+
+        if (!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+
+        const data = await response.json()
+        console.log(data)
+    }
+
+    catch(error){
+        console.error(error)
+    }
 }
 
 // Space Weather Database Of Notifications, Knowledge, Information (DONKI)
-function donki(date){
-    fetch (`https://api.nasa.gov/DONKI/FLR?startDate=${date}&endDate=${date}&api_key=${api_key}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+async function donki(date){
+
+    try{
+        const response = await fetch (`https://api.nasa.gov/DONKI/FLR?startDate=${date}&endDate=${date}&api_key=${api_key}`)
+
+        if (!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+
+        const data = await response.json()
+        console.log(data)
+    }
+
+    catch(error){
+        console.error(error)
+    }
 }
